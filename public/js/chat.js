5bf45f473fda462d541ddcf34de43ed4daf4959b
Koushik-Lakkysetty <lakky.19bce7279@vitap.ac.in>
2022-01-14 00:26:52 +0530

    feature-rendering link for location in UI

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,6 +9,7 @@ const $messages = document.querySelector('#messages')
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
 
 // socket.on('countUpdated', (count) => {
 //     console.log('count has been updated', count)
@@ -23,6 +24,14 @@ socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
         message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (locationURL) => {
+    console.log(locationURL)
+    const html = Mustache.render(locationTemplate, {
+        locationURL
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
